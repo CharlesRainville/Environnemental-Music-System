5a37e600effb67284b711d70b8caaf3f6563388f
Charles Rainville <46873146+CharlesRainville@users.noreply.github.com>
2019-02-17 16:52:19 -0500

    Add files via upload

---
+++ slip2InOut_arduinoIDEdigital_sensor.ino
@@ -0,0 +1,118 @@
+// Variables globales
+// Ces variables servent à l'encodage en SLIP.
+#define trigPin 13
+#define echoPin 12
+
+const byte END=192;
+const byte ESC=219; 
+const byte ESC_END=220;
+const byte ESC_ESC=221;
+
+const int sensorPin = 0; //L'entrée analogique utilisée.
+const int sensorPin2 = 1; //L'entrée analogique utilisée.
+const int outPins[] = { 9, 10, 11 }; // Les pins utilisées en sortie (PWM).
+
+// La taille maximum d'un paquet SLIP.  Ce nombre doit être plus grande que le
+// paquet le plus grand attendu.
+byte slipPacket[256]; // Cet array contiendra le paquet SLIP reçu.
+
+void setup() {
+  Serial.begin(115200);
+
+
+    pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);        
+  delayMicroseconds(2);              
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);           
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance >= 2000 || distance <= 0)
+  {
+    Serial.write(END);   // On commence le paquet.
+    SLIPSerialWrite(2000 >> 8); 
+    SLIPSerialWrite(2000 & 255); 
+    Serial.write(END); 
+  }
+  else 
+  {
+    // On lit le capteur et on envoie un paquet SLIP.
+    Serial.write(END);   // On commence le paquet.
+    SLIPSerialWrite( byte(distance >> 8) );  // On envoie le MSB
+    SLIPSerialWrite( byte(distance & 255) ); // On envoie le LSB
+    Serial.write(END);   // On termine le paquet.
+  }
+  
+	
+	delay(3);  // On attend un peu pour le port série et l'ADC. 
+}
+
+//Fonction pour encoder les paquets SLIP.
+void SLIPSerialWrite(byte value){
+  if(value == END) { // Si c'est la valeur 192, on remplace par 219 et 220.
+    Serial.write(ESC);
+    Serial.write(ESC_END);
+    return;
+  } else if (value == ESC) {  // Si c'est la valeur 219, on fait suivre un 221.
+    Serial.write(ESC);
+    Serial.write(ESC_ESC);
+    return;
+  } else { // On envoie toutes les autres valeurs normalement.
+    Serial.write(value);
+    return;
+  }
+}
+
+// Fonction pour recevoir et décoder les paquets SLIP.
+// Retourne la taille du paquet SLIP.
+// Prend un paramètre: le tableau (array) d'octets (bytes) qui contiendra le paquet SLIP.
+int SLIPSerialRead(byte * slipPacket) { 
+	int packetIndex = 0;  // L'index du paquet SLIP reçu.  Cette valeur sera
+	// incrémentée à chaque octet reçu.  Elle sera remise à
+	// zéro quand un paquet SLIP sera complet.
+
+	// Deux témoins (flags)
+	boolean escape = false; // Ce témoin devient vrai quand un octet ESC
+	// est reçu.  Il redevient faux quand l'octet
+	// suivant (ESC_ESC ou ESC_END) est reçu.
+	boolean packetComplete = false; // Ce témoin devient vrai quand un
+	// paquet SLIP est complet,
+	// c'est-à-dire quand l'octet END est
+	// reçu.
+
+  // S'il n'y a rien sur le port série, on retourne zéro immédiatement.
+  if ( Serial.available() == 0 ) {
+    return 0;
+  }
+
+  // S'il y a quelque chose, on attend un paquet complet.
+	while( !packetComplete ) {
+		if( Serial.available() > 0) {
+			byte b = Serial.read();
+			if (escape) { // Si l'octet précédent était ESC (219)
+				if (b == ESC_END) {  // On ajoute END (192)...
+					slipPacket[packetIndex] = END;
+				} else if (b == ESC_ESC) { // ...ou ESC (219)
+					slipPacket[packetIndex] = ESC;
+				}
+				packetIndex++;  
+				escape = 0; // On remet le témoin escape à faux.
+			} else if (b == END) { // Le paquet est terminé.
+				packetComplete = true;  // Le témoin packetComplete est maintenant vrai.
+			} else if (b == ESC) { // Si l'octet est ESC (219)
+				escape = 1; // Le témoin escape est maintenant vrai. (On n'incrémente pas packetIndex.)
+			} else { // Cas normal.
+				slipPacket[packetIndex] = b; // On ajoute l'octet au paquet et
+				packetIndex ++; // on incrémente packetIndex.
+			}
+		}
+	}
+  // On retourne la taille du paquet.
+  return packetIndex;
+}
